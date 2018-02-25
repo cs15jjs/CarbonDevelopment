f2d3839da6188ec385a68fb3e8a4cb15bb1e8340
Jo-Neil Starkie 1422088 <1422088@brunel.ac.uk>
2018-02-25 21:00:30 +0000

    Update of Initial Shell

--- ZeroEvents/app/src/main/java/carbon/zeroevents/MainActivity.java
+++ ZeroEvents/app/src/main/java/carbon/zeroevents/MainActivity.java
@@ -1,13 +1,55 @@
 package carbon.zeroevents;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    transaction.replace(R.id.content, new HomePageFragment()).commit();
+                    return true;
+                case R.id.navigation_explore:
+                    transaction.replace(R.id.content, new ExploreFragment()).commit();
+                    return true;
+                case R.id.navigation_connect:
+                    transaction.replace(R.id.content, new ConnectFragment()).commit();
+                    return true;
+                case R.id.navigation_settings:
+                    transaction.replace(R.id.content, new SettingsFragment()).commit();
+                    return true;
+            }
+            return false;
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home_page);
+
+        if (savedInstanceState == null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.content, new ExploreFragment()).commit();
+
+            BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+            navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        }
+
     }
 }
